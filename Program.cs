89d238f2777b503da744c19fdab2958e60026255
MatiasUe <110643079+MatiasUe@users.noreply.github.com>
2022-08-04 22:24:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Pessoa
+{
+
+    //propriedades
+    public string nome;
+    
+    public int idade;
+
+
+    public void apresentar_se(){
+
+        Console.WriteLine("Meu nome é"  +nome+ ", e tenho " +idade+ "anos de idade");
+
+    }
+
+}    
---
+++ pessoa.cs
@@ -0,0 +1,9 @@
+
+Pessoa xunda = new Pessoa();
+xunda.nome = "Xunda";
+xunda.idade = 12;
+
+xunda.apresentar_se();
+
+
+
